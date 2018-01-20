39884aec6d71e525d1fd585af99039de0740e222
Zushisan <74838@supinfo.com>
2018-01-20 20:02:28 +0000

    request response

--- https_example.js
+++ https_example.js
@@ -1,15 +1,14 @@
 const https = require('https');
 
-
-
 var options = {
   host: 'www.example.org',
   path: '/'
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +16,5 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end(); // CALLBACK MSG ONCE REQUEST IS DONE
 
 console.log("I've made the request!");
+
+
